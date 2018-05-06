5dbacb49e9d89176a9f829377ca7fa55ca61d5b2
CowDavid <31068867+CowDavid@users.noreply.github.com>
2018-05-06 17:20:12 +0800

    Update IG.v

--- IG.v
+++ IG.v
@@ -37,8 +37,8 @@ reg signed[9:0] img_y_grads [0:255];
 reg signed[9:0] img_y_grads_next [0:255];
 reg flag, flag_next;//For the store of the first row of pixels
 reg wr_flag, wr_flag_next;
-wire [255:0] index;
-wire [255:0] wr_index;
+wire [7:0] index;
+wire [7:0] wr_index;
 
 integer i;
 
@@ -51,9 +51,11 @@ assign grad_do = grad_do_tem;
 reg [15:0]img_addr_tem, grad_addr_tem;
 assign img_addr = img_addr_tem;
 assign grad_addr = grad_addr_tem;
-
-assign index = (img_addr_tem>255) ? (img_addr_tem)%256-1:img_addr_tem-1;
-assign wr_index = (grad_addr_tem>255) ? (grad_addr_tem+1)%256-1:grad_addr_tem;
+reg [7:0] img_di_tem;
+wire [7:0] img_di_next;
+assign img_di_next = img_di;
+assign index = (img_addr_tem>256) ? (img_addr_tem)%256-1:img_addr_tem-1;
+assign wr_index = (grad_addr_tem>256) ? (grad_addr_tem+1)%256-1+1:grad_addr_tem+1;
 //------------------------------------------------------------------
 // combinational part
 always@(*)begin
@@ -62,9 +64,9 @@ always@(*)begin
 	//wr_count_next = wr_count;
 	cal_count_next = cal_count;
 	y_count_next =  y_count;
-	img_addr_next = img_addr;
-	grad_addr_next = grad_addr;
-	grad_do_next = grad_do;
+	img_addr_next = img_addr_tem;
+	grad_addr_next = grad_addr_tem;
+	grad_do_next = grad_do_tem;
 	flag_next = flag;
 	wr_flag_next = wr_flag;
 	for(i = 0; i <= 255; i=i+1)begin
@@ -114,12 +116,12 @@ always@(*)begin
 			//count_next = count + 1;
 			if (flag == 1'b1)begin
 			//The first row has been completely stored
-				img_x_pixels1_next[index][7:0] = img_di;
+				img_x_pixels1_next[index][7:0] = img_di_tem;
 				img_x_pixels1_next[index][9:8] = 2'b00;
 			end
 			else begin
 			//The first row has not been completely stored
-				img_x_pixels0_next[index][7:0] = img_di;
+				img_x_pixels0_next[index][7:0] = img_di_tem;
 				img_x_pixels0_next[index][9:8] = 2'b00;
 			end
 			if ((img_addr_tem)%256 == 0)begin
@@ -160,18 +162,22 @@ always@(*)begin
 			grad_wr_next = 1'b1;
 			if(wr_flag == 1'b1)begin
 				grad_addr_next = grad_addr_tem + 1;
+				grad_do_next[19:10] = img_x_grads[wr_index];
+				grad_do_next[9:0] = img_y_grads[wr_index];
 			end
 			else begin
 				grad_addr_next = grad_addr_tem;
-				wr_flag_next = 1'b1; 
+				wr_flag_next = 1'b1;
+				grad_do_next[19:10] = img_x_grads[0];
+				grad_do_next[9:0] = img_y_grads[0];
 			end
 			//wr_count_next = wr_count + 1;
-			grad_do_next[19:10] = img_x_grads[wr_index];
-			grad_do_next[9:0] = img_y_grads[wr_index];
+			
 			if((grad_addr_tem + 1)%256 == 0)begin
 				if(y_count != 254)begin
 					y_count_next = y_count + 1;
 					state_next = READ0;
+					wr_flag_next = 1'b0;
 					for(i=0;i<=255;i=i+1)begin
 						img_x_pixels0_next[i] = img_x_pixels1[i];
 					end	
@@ -201,6 +207,7 @@ always @(posedge clk or posedge reset) begin
 		img_rd_tem <= 0;
 		img_addr_tem <= 0;
 		flag <= 0;
+		wr_flag <= 0;
 		//wr_count <= 0;
 		cal_count <= 0;
 		y_count <= 0;
@@ -209,6 +216,7 @@ always @(posedge clk or posedge reset) begin
 		img_addr_tem <= 0;
 		grad_addr_tem <= 0;
 		grad_do_tem <= 0;
+		img_di_tem <= img_di_next;
 		for(i = 0; i <= 255; i=i+1)begin
 			img_x_pixels0[i] <= 0;
 			img_x_pixels1[i] <= 0;
@@ -222,6 +230,7 @@ always @(posedge clk or posedge reset) begin
 		img_rd_tem <= img_rd_next;
 		img_addr_tem <= img_addr_next;
 		flag <= flag_next;
+		wr_flag <= wr_flag_next;
 		//wr_count <= wr_count_next;
 		cal_count <= cal_count_next;
 		y_count <= y_count_next;
@@ -230,6 +239,7 @@ always @(posedge clk or posedge reset) begin
 		img_addr_tem <= img_addr_next;
 		grad_addr_tem <= grad_addr_next;
 		grad_do_tem <= grad_do_next;
+		img_di_tem <= img_di_next;
 		for(i = 0; i <= 255; i=i+1)begin
 			img_x_pixels0[i] <= img_x_pixels0_next[i];
 			img_x_pixels1[i] <= img_x_pixels1_next[i];
